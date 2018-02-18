d670c0f0596bb84fc3e5f1b9bb24bf1c457ed7f8
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2018-02-17 22:42:57 -0500

    An idea

--- src/store/store.js
+++ src/store/store.js
@@ -9,16 +9,35 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
-    topics: {
-      titles: ['JavaScript', 'PHP', 'Laravel', 'Android'],
-      subtitles: ['Dynamic Frontend', 'Server King', 'Best in PHP', 'Most popular Mobile OS'],
-      descriptions: [
-        'JavaScript is the programming language of HTML and the Web. JavaScript is easy to learn.',
-        'PHP is a server scripting language, and a powerful tool for making dynamic web pages.',
-        'Love beautiful code? We do too. The PHP Framework For Web Artisans.',
-        'From phones and watches to cars and TVs, customise your digital life with Android.'
-      ]
-    }
+
+    topics: [{
+      title: 'JavaScript',
+      subtitle: 'Dynamic Frontend',
+      description: 'JavaScript is the programming language of HTML and the Web. JavaScript is easy to learn.'
+    }, {
+      title: 'PHP',
+      subtitle: 'Server King',
+      description: 'PHP is a server scripting language, and a powerful tool for making dynamic web pages.'
+    }, {
+      title: 'Laravel',
+      subtitle: 'Best in PHP',
+      description: 'Love beautiful code? We do too. The PHP Framework For Web Artisans.'
+    }, {
+      title: 'Android',
+      subtitle: 'Most popular Mobile OS',
+      description: 'From phones and watches to cars and TVs, customise your digital life with Android.'
+    }]
+
+    // topics: {
+    //   titles: ['JavaScript', 'PHP', 'Laravel', 'Android'],
+    //   subtitles: ['Dynamic Frontend', 'Server King', 'Best in PHP', 'Most popular Mobile OS'],
+    //   descriptions: [
+    //     'JavaScript is the programming language of HTML and the Web. JavaScript is easy to learn.',
+    //     'PHP is a server scripting language, and a powerful tool for making dynamic web pages.',
+    //     'Love beautiful code? We do too. The PHP Framework For Web Artisans.',
+    //     'From phones and watches to cars and TVs, customise your digital life with Android.'
+    //   ]
+    // }
   },
   getters,
   mutations,
